64b95e19c175878a8d0f24f036bd2d18446e5ae0
Matthew Berryhill <matthewberryhill@gmail.com>
2018-03-21 17:46:39 -0600

    Implement mock mine

--- main.go
+++ main.go
@@ -12,18 +12,20 @@ const TEST_FILE = "./test-logs.txt"
 const LOGS = "./logs.txt"
 
 func main () {
+
 	fmt.Println("This is the test mine")
 
 	t, _ := tail.TailFile(
 		TEST_FILE, tail.Config{Follow: true})
 	for line := range t.Lines {
 		fmt.Println(line.Text)
-		appendStringToFile(LOGS, line)
-		time.Sleep(time.Second)
+		appendStringToFile(LOGS, line.Text + "\n")
+		time.Sleep(time.Millisecond * 100)
 	}
 }
 
 func appendStringToFile(path, text string) error {
+	
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 	if err != nil {
 		return err
